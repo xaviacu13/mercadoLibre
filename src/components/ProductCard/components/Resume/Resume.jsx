580fde861961948ec016de10d874266aa251e647
xaviacu13 <40414894+xaviacu13@users.noreply.github.com>
2025-05-23 10:05:36 -0300

    setting ruotes and add views

---
+++ src/components/ProductCard/components/Resume/Resume.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+
+const Resume = () => {
+
+  return (
+    <div style={{ marginTop: '80px', padding: '20px' }}>
+      <Typography variant='h1' color='primary'>Resumen del producto</Typography>
+    </div>
+  );
+};
+
+export default Resume;
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+
+const Home = () => {
+  
+  return (
+    <div style={{ marginTop: '80px', padding: '20px' }}>
+      <Typography variant='h1' color='primary'>Bienvenidos a Mercado Libre</Typography>
+    </div>
+  );
+};
+
+export default Home;
